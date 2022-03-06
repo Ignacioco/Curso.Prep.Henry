e8f0d82907c2a158510de951e73d12a5397e4d71
Ignacio <ignaciocolombo84@gmail.com>
2022-03-06 12:13:33 -0300

    second commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = 'constancia';
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = null;
+const nuevoNum = perseverancia;
 
 // Crea una variable booleana:
 const nuevoBool = null;
